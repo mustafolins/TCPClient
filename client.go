7d3d0841041f668ac7a1608f8bd10d9df09004af
Jonathan Van Dam <mustafolins@gmail.com>
2018-11-24 23:32:37 -0700

    Updated Encryption
    Changed from GCM method to CFB

--- client.go
+++ client.go
@@ -4,9 +4,8 @@ import (
 	"bufio"
 	"crypto/aes"
 	"crypto/cipher"
-	"crypto/md5"
 	"crypto/rand"
-	"encoding/hex"
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -30,7 +29,8 @@ func main() {
 		// read in input from stdin
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Text to send: ")
-		text, _ := reader.ReadString('\x00')
+		text, _ := reader.ReadString('\n')
+		// text := "dude\n"
 		// send to socket
 		outgoingMessage := &jMessage{
 			Msg: text[:len(text)-1], // remove \x00 from message
@@ -39,54 +39,62 @@ func main() {
 		if err != nil {
 			continue
 		}
-		sendText := string(encrypt([]byte(outBytes), "password"))
-		fmt.Fprintf(conn, sendText+"\x00")
+		sendText := string(encrypt("password", []byte(outBytes)))
+		_, sendErr := fmt.Fprintf(conn, sendText+"\x00")
 		// listen for reply
-		message, _ := bufio.NewReader(conn).ReadString('\x00')
+		if sendErr == nil {
+			message, recErr := bufio.NewReader(conn).ReadString('\x00')
+			if recErr != nil {
+				continue
+			}
 
-		byt := decrypt([]byte(message[:len(message)-1]), "password")
-		var dat map[string]interface{}
-		json.Unmarshal(byt, &dat)
+			if len(message) > aes.BlockSize {
+				byt := decrypt("password", []byte(message[:len(message)-1]))
+				var dat map[string]interface{}
+				json.Unmarshal(byt, &dat)
 
-		fmt.Println("Message from server:", dat["message"].(string))
+				if dat != nil {
+					fmt.Println("Message from server:", dat["message"].(string))
+				}
+			}
+		}
 	}
 }
 
-func decrypt(data []byte, passphrase string) []byte {
-	key := []byte(createHash(passphrase))
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-	nonceSize := gcm.NonceSize()
-	nonce, ciphertext := data[:nonceSize], data[nonceSize:]
-	plaintext, err := gcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-	return plaintext
+func encodeBase64(b []byte) []byte {
+	return []byte(base64.StdEncoding.EncodeToString(b))
 }
 
-func encrypt(data []byte, passphrase string) []byte {
-	block, _ := aes.NewCipher([]byte(createHash(passphrase)))
-	gcm, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
+func decodeBase64(b []byte) []byte {
+	data, _ := base64.StdEncoding.DecodeString(string(b))
+	return data
+}
+
+func encrypt(key string, text []byte) []byte {
+	paddedKey := fmt.Sprintf("%032s", key)
+	block, _ := aes.NewCipher([]byte(paddedKey))
+
+	b := encodeBase64(text)
+	ciphertext := make([]byte, aes.BlockSize+len(b))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
 	}
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err = io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	cfb := cipher.NewCFBEncrypter(block, iv)
+	cfb.XORKeyStream(ciphertext[aes.BlockSize:], b)
 	return ciphertext
 }
 
-func createHash(key string) string {
-	hasher := md5.New()
-	hasher.Write([]byte(key))
-	return hex.EncodeToString(hasher.Sum(nil))
+func decrypt(key string, text []byte) []byte {
+	paddedKey := fmt.Sprintf("%032s", key)
+	block, _ := aes.NewCipher([]byte(paddedKey))
+
+	if len(text) < aes.BlockSize {
+		panic("ciphertext too short")
+	}
+	iv := text[:aes.BlockSize]
+	text = text[aes.BlockSize:]
+	cfb := cipher.NewCFBDecrypter(block, iv)
+	cfb.XORKeyStream(text, text)
+	return decodeBase64(text)
 }
